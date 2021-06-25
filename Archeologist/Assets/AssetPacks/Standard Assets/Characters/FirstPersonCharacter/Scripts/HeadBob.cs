7aeadf830be73b5854cd499e9f05bb02c02b4c5e
Shai Dickman <shai.j.dickman@gmail.com>
2021-06-24 20:47:44 -0700

    total restart using standard asset

---
+++ Archeologist/Assets/AssetPacks/Standard Assets/Characters/FirstPersonCharacter/Scripts/HeadBob.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class HeadBob : MonoBehaviour
+    {
+        public Camera Camera;
+        public RigidbodyFirstPersonController rigidbodyFirstPersonController;
+        public float StrideInterval;
+        [Range(0f, 1f)] public float RunningStrideLengthen;
+
+       // private CameraRefocus m_CameraRefocus;
+        private bool m_PreviouslyGrounded;
+        private Vector3 m_OriginalCameraPosition;
+
+
+        private void Start()
+        {
+            m_OriginalCameraPosition = Camera.transform.localPosition;
+       //     m_CameraRefocus = new CameraRefocus(Camera, transform.root.transform, Camera.transform.localPosition);
+        }
+
+
+        private void Update()
+        {
+          //  m_CameraRefocus.GetFocusPoint();
+            Vector3 newCameraPosition;
+            if (rigidbodyFirstPersonController.Velocity.magnitude > 0 && rigidbodyFirstPersonController.Grounded)
+            {
+                newCameraPosition = Camera.transform.localPosition;
+                newCameraPosition.y = Camera.transform.localPosition.y;
+            }
+            else
+            {
+                newCameraPosition = Camera.transform.localPosition;
+                newCameraPosition.y = m_OriginalCameraPosition.y;
+            }
+            Camera.transform.localPosition = newCameraPosition;
+
+            if (!m_PreviouslyGrounded && rigidbodyFirstPersonController.Grounded)
+            {
+            }
+
+            m_PreviouslyGrounded = rigidbodyFirstPersonController.Grounded;
+          //  m_CameraRefocus.SetFocusPoint();
+        }
+    }
+}
