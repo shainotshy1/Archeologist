329ef82a5ce76ed6d1f3f52355dd340fe3f0d445
Shai Dickman <shai.j.dickman@gmail.com>
2021-06-28 13:32:03 -0700

    rotation update - using local positions

--- Archeologist/Assets/Scripts/PathHandler.cs
+++ Archeologist/Assets/Scripts/PathHandler.cs
@@ -9,55 +9,88 @@ public class PathHandler : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] GameObject straightPath;
-    [SerializeField] GameObject rightTurn;
-    [SerializeField] GameObject leftTurn;
-    [SerializeField] GameObject forkPath;
     [SerializeField] float pathSeperatorHeight;
     [SerializeField] int pathsInfrontAmount;
 
     public static float pathSeperatorDistance;
+    float rotation;
     List<GameObject> platforms = new List<GameObject>();
+    System.Random random = new System.Random();
+    enum CurrentAction
+    {
+        Straight,LeftTurn,RightTurn,Fork
+    }
+    CurrentAction currentAction;
     private void Start()
     {
+        UpdatePathRotation();
         pathSeperatorDistance = straightPath.GetComponent<BoxCollider>().size.z;
 
         for (int i = -4; i <= pathsInfrontAmount; i++)
         {
             Vector3 newPosition = new Vector3(0, 0, pathSeperatorDistance*i);
-            GameObject addedPath = Instantiate(straightPath, newPosition, Quaternion.identity,transform);
+            GameObject addedPath = Instantiate(straightPath, newPosition, Quaternion.Euler(0,rotation,0),transform);
             platforms.Add(addedPath);
         }
     }
     private void Update()
     {
-        CreatePath();
+        UpdatePathRotation();
         MovePaths();
+        CreatePath();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
     }
 
+    private void UpdatePathRotation()
+    {
+        Transform player = FindObjectOfType<PlayerControls>().GetComponent<Transform>();
+        rotation = player.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
+    }
+
     private void MovePaths()
     {
-        Vector3 newPos = new Vector3(platforms[0].transform.position.x, platforms[0].transform.position.y, platforms[0].transform.position.z - Time.deltaTime * movementSpeed);
-        platforms[0].transform.position = newPos;
-        for(int i = 1; i < platforms.Count; i++)
+        Vector3 newPos = new Vector3(platforms[0].transform.localPosition.x, platforms[0].transform.localPosition.y, platforms[0].transform.localPosition.z - Time.deltaTime * movementSpeed);
+        platforms[0].transform.localPosition = newPos;
+        platforms[0].transform.rotation = Quaternion.Euler(0, rotation, 0);
+        for (int i = 1; i < platforms.Count; i++)
         {
-            newPos = new Vector3(platforms[i-1].transform.position.x, platforms[i-1].transform.position.y, platforms[i-1].transform.position.z + pathSeperatorDistance);
-            platforms[i].transform.position = newPos;
+            newPos = new Vector3(platforms[i-1].transform.localPosition.x, platforms[i-1].transform.localPosition.y, platforms[i-1].transform.localPosition.z +pathSeperatorDistance);
+            platforms[i].transform.localPosition = newPos;
+            platforms[i].transform.rotation = Quaternion.Euler(0, rotation, 0);
         }
     }
 
     private void CreatePath()
     {
-        if (platforms[0].transform.position.z < transform.position.z - 4*pathSeperatorDistance)
+        if (platforms[0].transform.localPosition.z< transform.localPosition.z - 4*pathSeperatorDistance)
         {
             platforms[0].GetComponent<PlatformHandler>().RemovePlatform();
             platforms.RemoveAt(0);
 
-            Vector3 newPosition = new Vector3(0, 0, pathSeperatorDistance+platforms[platforms.Count-1].transform.position.z);
-            GameObject addedPath = Instantiate(straightPath, newPosition, Quaternion.identity, transform);
+            int randomVal = (int)(random.NextDouble() * 100);
+
+            if (randomVal == 0)
+            {
+                currentAction = CurrentAction.LeftTurn;
+            }
+            else if(randomVal == 1){
+                currentAction = CurrentAction.RightTurn;
+            }
+            else if(randomVal == 2)
+            {
+                currentAction = CurrentAction.Fork;
+            }
+            else
+            {
+                currentAction = CurrentAction.Straight;
+            }
+
+            Vector3 newPosition = new Vector3(platforms[platforms.Count - 1].transform.localPosition.x, 0, pathSeperatorDistance + platforms[platforms.Count - 1].transform.localPosition.z);
+            GameObject addedPath = Instantiate(straightPath, newPosition, Quaternion.Euler(0, rotation, 0), transform);
             addedPath.GetComponent<PlatformHandler>().GenerateObstacle();
             platforms.Add(addedPath);
         }
